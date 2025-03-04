c0f6cf3b3bc93e1af09ede0011352a870b0bcc98
Saikorenji <eliec337@gmail.com>
2025-03-04 14:59:29 +0100

    Adding a souillure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,31 @@
-import { StyleSheet } from 'react-native';
-
-import { Text, View } from '@/components/Themed';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import { Text } from '@/components/Themed';
 import DreamForm from '@/components/DreamForm';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Sinj</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <DreamForm />
-    </View>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Sinj</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <DreamForm />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    width: '100%',
   },
   title: {
     fontSize: 20,
